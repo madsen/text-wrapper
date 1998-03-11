d976d18df5df6a1f4abfbcf91731eef00a4841ff
Christopher J. Madsen <perl@cjmweb.net>
1998-03-11 16:23:43 -0600

    Don't insert parStart between consecutive newlines
    Make sure result ends in a newline

--- Wrapper.pm
+++ Wrapper.pm
@@ -5,7 +5,7 @@ package Text::Wrapper;
 #
 # Author: Christopher J. Madsen <ac608@yfn.ysu.edu>
 # Created: 06 Mar 1998
-# Version: $Revision: 0.2 $ ($Date: 1998/03/09 06:36:27 $)
+# Version: $Revision: 0.3 $ ($Date: 1998/03/11 22:23:43 $)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the same terms as Perl itself.
@@ -29,7 +29,7 @@ use vars qw($VERSION);
 BEGIN
 {
     # Convert RCS revision number to d.ddd format:
-    $VERSION = sprintf('%d.%03d', '$Revision: 0.2 $ ' =~ /(\d+)\.(\d+)/);
+    $VERSION = sprintf('%d.%03d', '$Revision: 0.3 $ ' =~ /(\d+)\.(\d+)/);
 } # end BEGIN
 
 #=====================================================================
@@ -54,29 +54,30 @@ sub wrap
     my $width = $self->{'columns'};
     my $text = $self->{'parStart'};
     my $length = length $text;
-    my $parStart = "\n$text";
+    my $lineStart = $length;
+    my $parStart = $text;
     my $parStartLen = $length;
     my $continue = "\n" . $self->{'bodyStart'};
     my $contLen  = length $self->{'bodyStart'};
     while ($_[0] =~ m/(\s*\S+)/g) {
         my $word = $1;
       again:
-        if ($word =~ s/[ \t]*\n//) {
-            $text .= $parStart;
-            $length = $parStartLen;
+        if ($word =~ s/[ \t]*(\n+)//) {
+            $text .= $1 . $parStart;
+            $lineStart = $length = $parStartLen;
             goto again;
         } elsif ($length + length $word <= $width) {
             $length += length $word;
             $text .= $word;
         } else {
             $text .= $continue;
-            $length = $contLen;
-            $word =~ m/(\S+)/;
-            redo;
+            $lineStart = $length = $contLen;
+            $word =~ s/^\s+//;
+            goto again;
         }
     } # end while
-    if ($length) { $text .= "\n" }
-    else { $text =~ s/\Q$continue\E\Z/\n/ }
+    if ($length != $lineStart) { $text .= "\n" }
+    else { $text =~ s/(?:\Q$continue\E|\n\Q$parStart)\Z/\n/ }
 
     $text;
 }
